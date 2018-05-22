53ee9aecedd3fb040d24e5d6d157e0c511817542
Martin Hartt <martinhartt1@gmail.com>
2018-05-22 19:39:54 +0100

    Average words calculation

---
+++ dataset/average_words.js
@@ -0,0 +1,10 @@
+const { mean } = require('lodash');
+const { readFileSync } = require('fs');
+
+const ids = ['CAE', 'CPE', 'FCE', 'KET', 'PET']
+const raw = ids.map(name => readFileSync(`${name}.all.article.txt`).toString())
+const lengths = raw.map(file => file.split('\n').map(l => l.split(' ').length).filter(a => a > 1))
+
+const averages = lengths.map(mean)
+
+console.log(zipObject(ids, lengths.map(averages)))
